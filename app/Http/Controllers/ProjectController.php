c77be7145dde95f2ee9f57e6bba206664e99e950
Jaison Mora <jasonjairmoramora@gmail.com>
2023-10-12 23:36:22 -0500

    listado en selector combobox para edicion y creacion de compañia

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Company;
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -18,7 +21,11 @@ class ProjectController extends Controller
 
     public function create()
     {
-        return view('projects.create');
+        return view('projects.create', [
+            'companies' => Company::all(),
+            'cities' => City::all(),
+            'users' => User::all(),
+        ]);
     }
 
     public function store(Request $request)
@@ -33,7 +40,12 @@ class ProjectController extends Controller
 
     public function edit(Project $project)
     {
-        return view('projects.edit', compact('project'));
+        return view('projects.edit', [
+            'project' => $project,
+            'companies' => Company::all(),
+            'cities' => City::all(),
+            'users' => User::all(),
+        ]);
     }
 
     public function update(Request $request, string $id)
--- resources/views/projects/_form.blade.php
+++ resources/views/projects/_form.blade.php
@@ -7,4 +7,34 @@
     <input type="text" id="budget" name="budget" value="@isset($project) {{ $project->budget }} @endisset" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 ">
 </div>
 
+<div class="mb-6">
+    <label for="user" class="block mb-2 text-sm font-medium">Usuario</label>
+    <select id="user" name="user" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 ">
+        <option @empty($project) selected @endempty>Choose a user</option>
+        @foreach ($users as $user)
+            <option value="{{ $user->id }}" @if(isset($project) && $project->user->id === $user->id) selected @endif>{{ $user->name }}</option>    
+        @endforeach
+      </select>
+</div>
+
+<div class="mb-6">
+    <label for="city" class="block mb-2 text-sm font-medium">Ciudad</label>
+    <select id="city" name="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 ">
+        <option @empty($project) selected @endempty>Choose a country</option>
+        @foreach ($cities as $city)
+            <option value="{{ $city->id }}" @if(isset($project) && $project->city->id === $city->id) selected @endif>{{ $city->name }}</option>    
+        @endforeach
+      </select>
+</div>
+
+<div class="mb-6">
+    <label for="company" class="block mb-2 text-sm font-medium">Compañia</label>
+    <select id="company" name="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 ">
+        <option @empty($project) selected @endempty>Choose a company</option>
+        @foreach ($companies as $company)
+            <option value="{{ $company->id }}" @if(isset($project) && $project->company->id === $company->id) selected @endif>{{ $company->name }}</option>    
+        @endforeach
+      </select>
+</div>
+
 <button type="submit" class="uppercase text-white bg-yellow-500 hover:bg-yellow-600 font-bold rounded-lg text-xs px-5 py-2.5">Submit</submit>
